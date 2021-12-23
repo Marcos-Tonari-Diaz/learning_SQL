992b9f3eab69cbaac5fc8e0e6a2251a4a2425d5b
Marcos Diaz <marcos.dure.diaz@gmail.com>
2021-12-23 20:48:10 -0300

    Section 9 complete

--- cinema_database/tables.sql
+++ cinema_database/tables.sql
@@ -45,7 +45,7 @@ DESCRIBE screenings;
 
 CREATE TABLE seats (
 	id INT PRIMARY KEY AUTO_INCREMENT, 
-	row CHAR(1) NOT NULL,
+	seatrow CHAR(1) NOT NULL,
 	number INT NOT NULL,
 	room_id INT NOT NULL, 
 	FOREIGN KEY (room_id) REFERENCES rooms(id)
